375e51010821b0ecfa68e14bd3e257ec2c97b4a9
Vishnu Vardhan <vishnuvardhan1691@gmail.com>
2026-02-05 10:36:41 +0530

    Initial Files

---
+++ app.js
@@ -0,0 +1,16 @@
+function Samsung (){
+    console.log("This is Samsung")
+}
+function Apple (){
+    console.log("This is Apple")
+}
+function oppo (){
+    console.log("This is oppo")
+}
+function mi (){
+    console.log("This is mi")
+}
+Samsung()
+Apple()
+oppo()
+mi()
